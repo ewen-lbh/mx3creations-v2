185987277fd050c4e97ab3de988b8e073f793ef2
Ewen Le Bihan <ewen.lebihan7@gmail.com>
2019-06-10 20:19:42 +0200

    translations for contact form fields

--- corepages/forms.py
+++ corepages/forms.py
@@ -1,6 +1,7 @@
 from django import forms
+from django.utils.translation import gettext as _
 
 class ContactForm(forms.Form):
-    email = forms.EmailField(label='Email adress', required=True, widget=forms.TextInput(attrs={'placeholder':'Email adress'}))
-    message = forms.CharField(label="Your message", widget=forms.Textarea(attrs={'placeholder':'Your message'}), required=True)
-    bugreport = forms.BooleanField(label="Bug report/Feature request", required=False, help_text="Are you contacting me to suggest a feature or report a bug ?")
+    email = forms.EmailField(label=_('Email adress'), required=True, widget=forms.TextInput(attrs={'placeholder':_('Email adress')}))
+    message = forms.CharField(label=_("Your message"), widget=forms.Textarea(attrs={'placeholder':_('Your message')}), required=True)
+    bugreport = forms.BooleanField(label=_("Bug report/Feature request"), required=False, help_text=_("Are you contacting me to suggest a feature or report a bug ?"))
