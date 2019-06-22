e8adeca63a1bb7303d10e3cba0471f6e26a663db
ewen-lbh <ewen.lebihan7@gmail.com>
2019-06-22 14:33:38 +0200

    add function destroyCookie

--- static/corepages/scripts/cookies.js
+++ static/corepages/scripts/cookies.js
@@ -1,4 +1,5 @@
 function getCookie(cname) {
+    //console.log(`getCookie: requesting cookie ${cname}`)
     var name = cname + "=";
     var decodedCookie = decodeURIComponent(document.cookie);
     var ca = decodedCookie.split(';');
@@ -19,4 +20,10 @@ function setCookie(cname, cvalue, exdays) {
     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
     var expires = "expires=" + d.toUTCString();
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    console.log(`setCookie: cookie ${cname}=${cvalue}`)
+}
+
+function destroyCookie(cname) {
+    console.log(`destroyCookie: destroying cookie ${cname}`)
+    setCookie(cname, '', new Date(0).toUTCString())
 }
